3de927c1873aebc131b0464ae595cdc309e56ee0
Md Noman <noman_jahan@coredevs.ltd>
2024-01-04 12:18:47 +0600

    readme.md

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import'./App.css' 
+import Foo from './Components/Foo'
+
+function App() {
+  
+
+  return (
+    <>
+      <Foo bar="bar"></Foo>
+    </>
+  );
+}
+
+export default App
---
+++ src/Components/Foo.jsx
@@ -0,0 +1,18 @@
+const Foo = () => {
+    var a= 1;
+    return (
+        <div>
+            <h1>dh</h1>
+            <h1>{a}</h1>
+        </div>
+    );
+};
+
+export default Foo;
+
+
+
+
+
+
+
