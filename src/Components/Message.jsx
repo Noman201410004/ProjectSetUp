3244809bdc322e8b96dd181857b5297dcf30a86f
Md Noman <noman_jahan@coredevs.ltd>
2024-01-05 11:26:05 +0600

    UseMemo  Hook add

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,9 +1,19 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 
 const Message = ({ count,btnHandler }) => {
   console.log("Massage information")
+ const numbering=useMemo(()=>{
+    let number=0;
+            for(let i=0;i<=50000000;i++)
+            {
+              number++
+            }
+
+            return number;
+  },[])
   return (
     <div>
+    <h1>numbering:{numbering}</h1>
       <h1>Message Count: {count} information</h1>
       <button onClick={btnHandler}>Increase Number of Massage</button>
     </div>
